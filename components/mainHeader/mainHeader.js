5b91cbcba0f09cf5697d0f62c7ac7785c7c6ee8b
KIMCHOONGMAN <cm730712@gmail.com>
2024-06-13 22:54:14 +0900

    [Add] 메인 헤더 작업

---
+++ components/mainHeader/mainHeader.js
@@ -0,0 +1,30 @@
+import Link from "next/link";
+import Image from "next/image";
+import logoImg from "@/assets/logo.png";
+import classes from "./mainHeader.module.css";
+import MainHeaderBackground from "./mainHeader-background";
+import NavLink from "./navLink";
+
+export default function MainHeader() {
+  return (
+    <>
+      <MainHeaderBackground />
+      <header className={classes.header}>
+        <Link className={classes.logo} href="/">
+          <Image src={logoImg} alt="A plate with food on it" priority />
+          NextLevel Food
+        </Link>
+        <nav className={classes.nav}>
+          <ul>
+            <li>
+              <NavLink href="/meals">Browse Meals</NavLink>
+            </li>
+            <li>
+              <NavLink href="/community">Foodies Community</NavLink>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+}
