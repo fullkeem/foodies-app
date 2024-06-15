106a173a2a8ddd132c43405560431424abb45124
KIMCHOONGMAN <cm730712@gmail.com>
2024-06-16 00:31:03 +0900

    [Feat] 레시피 공유 폼 제출 기능 구현

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,5 +1,6 @@
-import ImagePicker from "@/components/meals/imagePicker";
 import classes from "./page.module.css";
+import { shareMyMeal } from "@/lib/actions";
+import ImagePicker from "@/components/meals/imagePicker";
 
 export default function ShareMeal() {
   return (
@@ -11,7 +12,7 @@ export default function ShareMeal() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form}>
+        <form className={classes.form} action={shareMyMeal}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -39,7 +40,7 @@ export default function ShareMeal() {
               required
             ></textarea>
           </p>
-          <ImagePicker />
+          <ImagePicker label="Your image" name="image" />
           <p className={classes.actions}>
             <button type="submit">Share Meal</button>
           </p>
